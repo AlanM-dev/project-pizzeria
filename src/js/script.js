21cce03c2164308bfbcb56385a1ec27dc265acf1
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-07-30 14:53:09 +0200

    Addclass Cart

--- src/js/script.js
+++ src/js/script.js
@@ -305,6 +305,30 @@
 
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('New CART:', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+
+
+
+
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -325,6 +349,13 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       // console.log('*** App starting ***');
@@ -335,6 +366,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
