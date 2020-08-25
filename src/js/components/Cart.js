f99cce3327640f98c08a5f41467337bba690d18a
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-08-25 15:40:19 +0200

    Fix add components // Module 10.1 - DONE

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,5 +1,6 @@
-import {utils, templates, settings, classNames, select} from './settings.js';
-import CartProduct from './components/CartProduct.js';
+import {templates, settings, classNames, select} from '../settings.js';
+import utils from '../utils.js';
+import CartProduct from './CartProduct.js';
 
 class Cart{
   constructor(element){
